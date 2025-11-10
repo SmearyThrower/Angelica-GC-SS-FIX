1bdd706603d4baa8762f72d0f73579623e52ae93
SmearyThrower <93036561+SmearyThrower@users.noreply.github.com>
2025-11-10 15:39:44 +0800

    Update MixinClientProxyCore.java

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/galacticraft/MixinClientProxyCore.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/galacticraft/MixinClientProxyCore.java
@@ -8,11 +8,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
-@Mixin(targets = "micdoodle8.mods.galacticraft.core.client.render.entities.RenderPlayerGC", remap = false)  // Updated target to RenderPlayerGC
+@Mixin(targets = "micdoodle8.mods.galacticraft.core.client.render.entities.RenderPlayerGC", remap = false)
 public class MixinClientProxyCore {
 
     @Redirect(
-        method = "doRender",  // Updated method to doRender (common in renderers); adjust if needed
+        method = "doRender",
         at = @At(
             value = "INVOKE",
             target = "Lorg/lwjgl/opengl/GL11;glRotatef(FFFF)V",
@@ -24,7 +24,7 @@ public class MixinClientProxyCore {
         // Debug logging to confirm mixin execution
         System.out.println("[DEBUG Angelica GC Fix] Mixin triggered! Config: " + CompatConfig.fixGalacticraftGravityTurn);
         
-        // Temporarily force skip for testing (always skip the GL rotation)
+        // Force skip for testing (always skip the GL rotation)
         System.out.println("[DEBUG] GL rotation skipped!");
         // If config is enabled (true), skip the rotation entirely (no-op, like commenting it out)
         // This fixes spacestation rendering in Angelica by disabling the gravity turn GL rotation
